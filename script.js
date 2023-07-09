e83e74424a9dddf1a9a01580509a389266a5d1b6
Brahm Dev Pandey <brahmdevpandey762@gmail.com>
2023-07-09 23:15:18 +0530

    some changes

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
-const addButton = document.querySelector('#add');
-const addNewNote = (text = '') => {
-    const note= document.createElement('div');
-    note.classList.add('note');
-    const htmlData = ` <div class="operation">
+const addButton = document.querySelector("#add");
+const addNewNote = (text = "") => {
+  const note = document.createElement("div");
+  note.classList.add("note");
+  const htmlData = ` <div class="operation">
     <button class="edit"><i class="fa-solid fa-pen-to-square"></i></button>
     <button class="delete"><i class="fa-solid fa-trash"></i></button>
 
 </div>
  <div class="main ${text ? "hidden" : ""}"></div>
- <textarea class="${text ? "hidden" : ""}"></textarea>`
- note.insertAdjacentHTML('afterbegin',htmlData)
- console.log(note);
+ <textarea class="${text ? "hidden" : ""}"></textarea>`;
+  note.insertAdjacentHTML("afterbegin", htmlData);
+  console.log(note);
 
-const editButton = note.querySelector('.edit')
-const delButton = note.querySelector('.delete')
-const mainButton = note.querySelector('.main')
-const textarea = note.querySelector('textarea')
+  const editButton = note.querySelector(".edit");
+  const delButton = note.querySelector(".delete");
+  const mainButton = note.querySelector(".main");
+  const textarea = note.querySelector("textarea");
 
-delButton.addEventListener('click', () =>{ 
+  delButton.addEventListener("click", () => {
     note.remove();
-})
-textarea.value = text;
-mainButton.innerHTML =text;
+  });
 
-editButton.addEventListener('click', () => {
-   mainButton.classList.toggle('hidden')
-   textarea.classList.toggle('hidden')
-})
- textarea.addEventListener('change',(event) => {
+  textarea.value = text;
+  mainButton.innerHTML = text;
+
+  editButton.addEventListener("click", () => {
+    mainButton.classList.toggle("hidden");
+    textarea.classList.toggle("hidden");
+  });
+  textarea.addEventListener("change", (event) => {
     const value = event.target.value;
     // console.log(value)
     mainButton.innerHTML = value;
-    
- })
+    textarea.value = "";
+  });
 
+  document.body.appendChild(note);
+};
 
- document.body.appendChild(note);
-
-}
- 
-addButton.addEventListener('click', () => addNewNote())
+addButton.addEventListener("click", () => addNewNote());
